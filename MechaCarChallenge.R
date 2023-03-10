7fdda2eee03fbb066c83d94d607e7d5c2162cc2d
stacey1079 <45715246+stacey1079@users.noreply.github.com>
2023-03-10 14:47:25 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,3 +6,7 @@ df <- read.csv(file="MechaCar_mpg.csv")
 reg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=df)
 summary(reg)
 
+# Import Suspension_Coil.csv
+sus_coil <- read.csv(file = "Suspension_Coil.csv")
+
+total_summary <- summarize(sus_coil, mean(PSI), median(PSI), var(PSI), sd(PSI))
